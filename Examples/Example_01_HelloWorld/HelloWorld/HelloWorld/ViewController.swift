9745bdfcd6086735e7b59624cbdc0edf00cd5d5e
Enric Sánchez <enric.sanchez85@gmail.com>
2016-10-18 09:25:06 +0200

    Add Example 01

---
+++ Examples/Example_01_HelloWorld/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Enric Sánchez on 17/10/16.
+//  Copyright © 2016 axa. All rights reserved.
+//
+
+import UIKit
+
+let defaultValue = "Hello World!"
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var lbHello: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lbHello.text = defaultValue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btPressed(_ sender: AnyObject) {
+        if let name = tfName.text {
+            lbHello.text = (name.characters.count > 0) ? "Hello \(name)!" : defaultValue
+        }
+    }
+    
+}
+
