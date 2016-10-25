94e87f65905d341cb20e10caddceaa26d0838b31
Enric Sánchez <enric.sanchez.altran@axa-groupsolutions.com>
2016-10-25 12:33:10 +0200

    Update swift sintax with comments and more details

--- Examples/Example_00_Swift/MyPlayground.playground/Contents.swift
+++ Examples/Example_00_Swift/MyPlayground.playground/Contents.swift
@@ -1,117 +1,442 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+print("Hello world!")
 
-///////////
-//Variables
-///////////
+////////////////
+//Simple Values
+////////////////
 
-let str = "Hello"
-//str = "Ouch!"
+//Use let to make a constant and var to make a variable
+var myVariable = 22
+myVariable = 27
 
-var mutableInt = 3
-mutableInt = 4
+let myConstant = 26
 
-var number: Float
-let string: String = "Initial value"
+//If the initial value doesn’t provide enough information (or if there is no initial value), specify the type by writing it after the variable, separated by a colon.
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
 
-/////////
-///Enums
-/////////
+//Values are never implicitly converted to another type. If you need to convert a value to a different type, explicitly make an instance of the desired type.
+let text = "The width is "
+let width = 94
+let widthText = text + String(width)
 
-enum Animal: Int {
-    case dog = 1
-    case cat = 2
+//There’s an even simpler way to include values in strings: Write the value in parentheses, and write a backslash (\) before the parentheses. For example:
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+//Create arrays and dictionaries using brackets ([]), and access their elements by writing the index or key in brackets. A comma is allowed after the last element.
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "Public Relations"
+
+//To create an empty array or dictionary, use the initializer syntax.
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+
+
+
+////////////////
+//Control Flow
+////////////////
+
+//Use if and switch to make conditionals, and use for-in, for, while, and repeat-while to make loops. Parentheses around the condition or loop variable are optional. Braces around the body are required.
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
 }
-var type = Animal.cat
+print(teamScore)
 
-/////////
-//Strings
-/////////
+//Switches support any kind of data and a wide variety of comparison operations—they aren’t limited to integers and tests for equality.
+let vegetable = "red pepper"
 
-var nameOne = "Ray"
-var nameTwo = "Brian"
-var combinedNames = "\(nameOne) and \(nameTwo)!"
-var tipString = "2499"
-var tipInt = Int(tipString)
-tipString = "24.99"
-var tipDouble = Double(tipString)
-
-///////
-//Arrays
-///////
-
-var arr = [nameOne, nameTwo]
-arr.append("Roman")
-
-for name in arr {
-    print(name)
-}
-
-//////
-//Dicts
-//////
-
-var dict:[String: String] = ["key1": "value1",
-                             "key2": "value2",
-                             "key3": "value3" ]
-dict["key3"] = "newValue3"
-dict["key2"] = nil
-
-for (key, value) in dict {
-    print("key: \(key), value: \(value)")
-}
-
-//////
-//Flow
-//////
-
-if mutableInt == 4 {
-    print("ok")
-}
-
-switch str {
-    case "Hello":
-        print("isHello")
-        break
-    case "Bye":
-        print("isntHello")
-        break
+switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log.")
+    case "cucumber", "watercress":
+        print("That would make a good tea sandwich.")
+    case let x where x.hasSuffix("pepper"):
+        print("Is it a spicy \(x)?")
     default:
-        print("")
+        print("Everything tastes good in soup.")
 }
 
-for i in 0..<3 {
-    mutableInt += 1
+//You use for-in to iterate over items in a dictionary by providing a pair of names to use for each key-value pair. Dictionaries are an unordered collection, so their keys and values are iterated over in an arbitrary order.
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+//Use while to repeat a block of code until a condition changes. The condition of a loop can be at the end instead, ensuring that the loop is run at least once.
+var n = 2
+while n < 100 {
+    n = n * 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m = m * 2
+} while m < 100
+print(m)
+
+//You can keep an index in a loop by using ..< to make a range of indexes.
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+
+////////////////
+//Optionals
+////////////////
+
+/*
+ You can use if and let together to work with values that might be missing. These values are represented as optionals. An optional value either contains a value or contains nil to indicate that a value is missing. Write a question mark (?) after the type of a value to mark the value as optional.
+*/
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
 }
 
-print(mutableInt)
+/*
+ If the optional value is nil, the conditional is false and the code in braces is skipped. Otherwise, the optional value is unwrapped and assigned to the constant after let, which makes the unwrapped value available inside the block of code.
+ 
+ Another way to handle optional values is to provide a default value using the ?? operator. If the optional value is missing, the default value is used instead.
+ */
 
-///Functions
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
 
-func saySomething() {
-    print("Something")
+
+////////////////
+//Functions and Closures
+////////////////
+
+/*
+Use func to declare a function. Call a function by following its name with a list of arguments in parentheses.
+Use -> to separate the parameter names and types from the function’s return type.
+ */
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
 }
 
-func say(message: String) {
-    print(message)
+greet(person: "Bob", day: "Tuesday")
+
+
+//By default, functions use their parameter names as labels for their arguments. Write a custom argument label before the parameter name, or write _ to use no argument label.
+ 
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
 }
+greet("John", on: "Wednesday")
 
-func sayThis(_ message: String) {
-    print(message)
+//Use a tuple to make a compound value—for example, to return multiple values from a function. The elements of a tuple can be referred to either by name or by number.
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
 }
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
 
-say(message: "Swift")
-say(message: "Swift another time")
-sayThis("Swift rocks")
+//Functions can also take a variable number of arguments, collecting them into an array.
 
-func checkNumber(number: Int) -> Bool {
-    if number == 3 {
-        return true
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+sumOf()
+sumOf(numbers: 42, 597, 12)
+
+//Functions can be nested. Nested functions have access to variables that were declared in the outer function. You can use nested functions to organize the code in a function that is long or complex.
+
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+//Functions are a first-class type. This means that a function can return another function as its value.
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+//A function can take another function as one of its arguments.
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
     }
     return false
 }
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
 
-checkNumber(number: 3)
-checkNumber(number: 5)
+//You can write a closure without a name by surrounding code with braces ({}). Use in to separate the arguments and return type from the body.
 
+numbers.map({
+    (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+//You have several options for writing closures more concisely. When a closure’s type is already known, such as the callback for a delegate, you can omit the type of its parameters, its return type, or both. Single statement closures implicitly return the value of their only statement.
+
+let mappedNumbers = numbers.map({ number in 3 * number })
+print(mappedNumbers)
+
+//You can refer to parameters by number instead of by name—this approach is especially useful in very short closures. A closure passed as the last argument to a function can appear immediately after the parentheses. When a closure is the only argument to a function, you can omit the parentheses entirely.
+
+let sortedNumbers = numbers.sorted { $0 > $1 }
+print(sortedNumbers)
+
+
+////////////////
+//Objects and Classes
+////////////////
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+//Create an instance of a class by putting parentheses after the class name. Use dot syntax to access the properties and methods of the instance.
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+//This version of the Shape class is missing something important: an initializer to set up the class when an instance is created. Use init to create one.
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+//Methods on a subclass that override the superclass’s implementation are marked with override
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() ->  Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+//In addition to simple properties that are stored, properties can have a getter and a setter.
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+
+////////////////
+//Enumerations and Structures
+////////////////
+
+//Use enum to create an enumeration. Like classes and all other named types, enumerations can have methods associated with them.
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+//Use the init?(rawValue:) initializer to make an instance of an enumeration from a raw value.
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
+
+//The case values of an enumeration are actual values, not just another way of writing their raw values. In fact, in cases where there isn’t a meaningful raw value, you don’t have to provide one.
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+//Use struct to create a structure. Structures support many of the same behaviors as classes, including methods and initializers. One of the most important differences between structures and classes is that structures are always copied when they are passed around in your code, but classes are passed by reference.
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+////////////////
+//Protocols and Extensions
+////////////////
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+//Classes, enumerations, and structs can all adopt protocols.
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted."
+    }
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+//Use extension to add functionality to an existing type, such as new methods and computed properties. You can use an extension to add protocol conformance to a type that is declared elsewhere, or even to a type that you imported from a library or framework.
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+print(7.simpleDescription)
